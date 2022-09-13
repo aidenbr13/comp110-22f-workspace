46cd2dde7d1b0eea66d6f7e3a10c033ebd2c63f3
Aiden Brosterhous <aidenbr@live.unc.edu>
2022-09-13 12:55:12 -0400

    commit

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,20 +1,22 @@
-"""EX03 - Wordle"""
+"""EX03 - Wordle!"""
 
 __author__: str = "730320104"
 
-def contains_char(word: str, character: str) -> str:
-    """Defining a function that tests if a character is found in a given word"""
+
+def contains_char(word: str, character: str) -> bool:
+    """Defining a function that tests if a character is found in a given word."""
     assert len(character) == 1
     i: int = 0
     while i < len(word):
         if character == word[i]:
             return True
         else:
-            i= i + 1
+            i = i + 1
     return False
 
+
 def emojified(predict: str, secret: str) -> str:
-    """Defining a function that prints emojis that correlate to the characters of a guess in relation to a secret word"""
+    """Defining a function that prints emojis that correlate to the characters of a guess in relation to a secret word."""
     assert len(predict) == len(secret)
     i: int = 0
     white_box: str = "\U00002B1C"
@@ -23,26 +25,29 @@ def emojified(predict: str, secret: str) -> str:
     emoji = ''
     while i < len(secret):
         if predict[i] == secret[i]:
-            i= i + 1
+            i = i + 1
             emoji += green_box
         else:
-            if contains_char(secret, predict[i]) == True:
+            if contains_char(secret, predict[i]) is True:
                 emoji += yellow_box
             else:
                 emoji += white_box
-            i= i + 1
+            i = i + 1
     return emoji
 
-def input_guess(length: int) -> int:
+
+def input_guess(length: int) -> str:
+    """Created a function that confirms the correct number of characters were guessed by the player."""
     guess: str = input(f"Enter a {length} character word: ")
     while len(guess) != length:
         guess = input(f"That wasn't {length} chars! Try again: ")
     else:
-        return(guess)
+        return (guess)
+
 
 def main() -> None:
     """The entrypoint of the program and main game loop."""
-    n: int= 1
+    n: int = 1
     print(f"=== Turn {n}/6 ===")
     wordle: str = "codes"
     guessing: str = input_guess(5)
@@ -53,11 +58,12 @@ def main() -> None:
             return
         if guessing != wordle:
             print(emojified(guessing, wordle))
-            n= n + 1
+            n = n + 1
             print(f"=== Turn {n}/6 ===")
             guessing = input_guess(5)
     print(emojified(guessing, wordle))
     print("X/6 - Sorry, try again tomorrow!")
+
 
 if __name__ == "__main__":
     main()
