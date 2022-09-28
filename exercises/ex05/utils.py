24a6e27171cacf187c0b6cb63e7382fa2b894d63
Aiden Brosterhous <aidenbr@live.unc.edu>
2022-09-28 15:30:05 -0400

    commit

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -5,20 +5,25 @@ __author__: str = "730320104"
 
 def only_evens(x: list[int]) -> list[int]:
     """Defined a function that returns even numbers in a given list."""
+    i: int = 0
     z: list[int] = []
-    for i in x:
+    while i < len(x):
         if x[i] % 2 == 0:
             z.append(x[i])
+        i += 1
     return z
 
 
 def concat(x: list[int], y: list[int]) -> list[int]:
     """Defined a function that combines the elements of two lists into one longer list."""
     z: list[int] = []
-    for i in x:
+    i: int = 0
+    while i < len(x):
         z.append(x[i])
-    for y[i] in y:
+        i += 1
+    while i < len(y):
         z.append(y[i])
+        i += 1
     return z
 
 
