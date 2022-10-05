45b36691b7d339c25cfb674c4f1b06a6e9ebe324
Aiden Brosterhous <aidenbr@live.unc.edu>
2022-10-05 15:59:16 -0400

    commit

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -4,27 +4,35 @@ __author__: str = "730320104"
 
 from random import randint
 
-secret: int = randint(1, 100)
-
-player: str = input("Hi, and welcome to NUMBER GOLF, the game show where you guess a randomly generated integer between 1 and 100 in the least amount of attempts. The goal of the game is to correctly guess the number and have the least amount of strokes. What is your name, contestant? ") 
+secret: int = 50
 
 x: int = 0
 
-def greet(player: str) -> None:
+player: str = ""
+
+points: int = 0
+
+keep_playing: int = 0
+
+
+def greet() -> None:
     """Defined a function that greets a player when joining the game and helps them understand their first decision."""
-    points: int = 0
+    global points
+    global player
+    print("Hi, and welcome to NUMBER GOLF, the game show where you guess a randomly generated integer between 1 and 100 in the least amount of attempts. The goal of the game is to correctly guess the number and have the least amount of strokes. What is your name, contestant? ") 
+    player = input()
     print(f"Ok {player}, let's begin the game! You have 3 options for your first 'shot': a) guess a random integer (+1 stroke), b) ask for a hint (+2 strokes), or c) give up (+1000 strokes). Please type your selection now: (a/b/c) ")
     y: str = input()
     if y == "a":
         points = points + 1
-        f = int(input(f"What is your integer guess? "))
+        f = int(input("What is your integer guess? "))
         if f < secret:
             print("Not quite. Guess a higher number.")
         if f > secret:
             print("Not quite. Guess a lower number.")
         if f == secret:
-            print(f"You got it! Congratulations! Your total strokes was {points}.")
-            quit()
+            print(f"Hole in one! Congratulations! Your total strokes was {points}.")
+            #quit()
     if y == "b":
         points = points + 2
         if secret <= 50:
@@ -34,41 +42,38 @@ def greet(player: str) -> None:
     if y == "c":
         points = points + 1000
         print(f"Thank you for playing. Your total strokes was {points}.")
-        quit()
+        return points
 
-
-def main() -> None:
-    """Defined the 'main' function of our game that runs and lets the player try to pick the secret number."""
-    points: int = 0
-    greet(player)
-    hint()
-    points += 2
-    while guess(x) != secret:
-        guess(x)
-        points += 1
-    else:
-        return guess(x)
 
 def guess(z: int) -> int:
     """Defined the guess function for the game, which allows players to make multiple guesses for the secret number."""
+    global points
+    global player
+    global keep_playing
     print(f"Now {player}, make your next integer guess (+1 stroke): ")
     z: int = int(input())
     if z < secret:
+        points = points + 1
         print("Not quite. Guess a higher number.")
     if z > secret:
+        points = points + 1
         print("Not quite. Guess a lower number.")
     if z == secret:
-        print(f"You got it! Congratulations! Your total strokes were ")
-        quit()
+        points = points + 1
+        #keep_playing += 1
+        print(f"You got it! Congratulations! Your total strokes were {points}.")
 
 
 def hint() -> None:
     """Defined a hint function for the game that allows the player an option to take a hint as their second turn."""
+    global points
+    global player
     GREEN_BOX: str = "\U0001F7E9"
     YELLOW_BOX: str = "\U0001F7E8"
     print(f"{player}, you have the option of taking another hint (+2 strokes). The hint will tell you if the secret number is even (green) or odd (yellow). If you would like to take the hint, input a 'y' now. ")
     answer: str = input()
     if answer == "y":
+        points = points + 2
         if secret % 2 == 0:
             print(GREEN_BOX)
         else:
@@ -77,5 +82,23 @@ def hint() -> None:
         return
 
 
+def main() -> None:
+    """Defined the 'main' function of our game that runs and lets the player try to pick the secret number."""
+    global points
+    global player
+    global keep_playing
+    while keep_playing == 0:
+        greet()
+        while points < 2:
+            hint()
+        while points < 10:
+            guess(x)
+        input(f"{player}, great job. Would you like to play again? Input the 'y' and 'ENTER' keys twice if yes and the 'n' and 'ENTER' keys twice if no: ")
+        if input() == "n":
+            keep_playing += 1
+
+
 if __name__ == "__main__":
     main()
+
+    # struggling with player variable and game loop (quit)
